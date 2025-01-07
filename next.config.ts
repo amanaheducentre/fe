908baa3505ff3d1e2795d22a0a13fce322dcd698
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2025-01-07 11:59:12 +0700

    Configure github deploy env

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
+import pj from "./package.json";
+
+const hostingEnv = process.env.HOSTING_ENV;
+let basePath = "";
+let assetPrefix = "";
+
+if (hostingEnv == "GH_PAGES") {
+  basePath = `/${pj.name}`;
+  assetPrefix = `/${pj.name}`;
+}
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  reactStrictMode: true,
+  basePath: basePath,
+  assetPrefix: assetPrefix,
 };
 
 export default nextConfig;
