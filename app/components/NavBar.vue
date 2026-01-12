f175bbdf4a8f9e2607c521edd7acd5d4705236ed
Dicky Mulia Fiqri <santrialanwari@gmail.com>
2026-01-12 11:27:08 +0700

    Migrate to bun

--- app/components/NavBar.vue
+++ app/components/NavBar.vue
@@ -5,125 +5,119 @@ const isMenuOpen = ref(false);
 const showCloseMenu = ref(false);
 
 watch(isMenuOpen, async () => {
-    showCloseMenu.value = false;
-    if (isMenuOpen.value == true) {
-        await sleep(400);
-        showCloseMenu.value = true;
-    }
+  showCloseMenu.value = false;
+  if (isMenuOpen.value == true) {
+    await sleep(400);
+    showCloseMenu.value = true;
+  }
 });
 
 onMounted(() => {
-    if (loggedIn.value == true) {
-        navbarMenu.value[0] = [
-            {
-                id: 1001,
-                name: "Log Out",
-                to: "/logout",
-                class: "text-raka-blue",
-            },
-        ];
-    } else {
-        navbarMenu.value[0] = [
-            {
-                id: 1001,
-                name: "Log In",
-                to: "/login",
-                class: "text-raka-blue",
-            },
-        ];
-    }
+  if (loggedIn.value == true) {
+    navbarMenu.value[0] = [
+      {
+        id: 1001,
+        name: "Log Out",
+        to: "/logout",
+        class: "text-raka-blue",
+      },
+    ];
+  } else {
+    navbarMenu.value[0] = [
+      {
+        id: 1001,
+        name: "Log In",
+        to: "/login",
+        class: "text-raka-blue",
+      },
+    ];
+  }
 });
 
 const navbarMenu = ref([
-    [],
-    [
-        {
-            id: 2001,
-            name: "Main Menu",
-            to: "#",
-            class: "text-xl text-raka-orange",
-        },
-        {
-            id: 2002,
-            name: "Program",
-            to: "#",
-            class: "text-sm",
-        },
-        {
-            id: 2003,
-            name: "Gallery",
-            to: "#",
-            class: "text-sm",
-        },
-        {
-            id: 2004,
-            name: "Beasiswa",
-            to: "#",
-            class: "text-sm",
-        },
-    ],
+  [],
+  [
+    {
+      id: 2001,
+      name: "Main Menu",
+      to: "#",
+      class: "text-xl text-raka-orange",
+    },
+    {
+      id: 2002,
+      name: "Program",
+      to: "#",
+      class: "text-sm",
+    },
+    {
+      id: 2003,
+      name: "Gallery",
+      to: "#",
+      class: "text-sm",
+    },
+    {
+      id: 2004,
+      name: "Beasiswa",
+      to: "#",
+      class: "text-sm",
+    },
+  ],
 ]);
 </script>
 
 <template>
-    <!-- Mobile Navigation -->
-    <div v-if="isMobile" class="grid grid-cols-3 w-full h-16 bg-white">
-        <div class="flex items-center px-3">
-            <UDrawer>
-                <UButton variant="outline" class="ring-0 text-black">
-                    <Icon name="qlementine-icons:menu-burger-16" width="24" />
-                </UButton>
+  <!-- Mobile Navigation -->
+  <div v-if="isMobile" class="grid grid-cols-3 w-full h-16 bg-white">
+    <div class="flex items-center px-3">
+      <UDrawer>
+        <UButton variant="outline" class="ring-0 text-black">
+          <Icon name="qlementine-icons:menu-burger-16" width="24" />
+        </UButton>
 
-                <template #content>
-                    <SideBar />
-                </template>
-            </UDrawer>
-        </div>
-        <div class="flex justify-center items-center">
-            <NuxtLink to="/">
-                <NuxtImg src="/img/logo.png" class="w-7" />
-            </NuxtLink>
-        </div>
+        <template #content>
+          <SideBar />
+        </template>
+      </UDrawer>
     </div>
+    <div class="flex justify-center items-center">
+      <NuxtLink to="/">
+        <NuxtImg src="/img/logo.png" class="w-7" />
+      </NuxtLink>
+    </div>
+  </div>
 
-    <!-- Desktop Navigation -->
-    <div v-else class="grid grid-cols-3 w-full h-24 bg-white">
-        <div class="flex items-center">
-            <NuxtLink to="/">
-                <NuxtImg src="/img/logo.png" width="32" class="ml-6" />
-            </NuxtLink>
-        </div>
-        <div />
-        <div class="flex justify-end items-center">
-            <div class="flex h-full justify-center items-center gap-3 mx-4">
-                <NuxtLink to="#"> PROGRAM </NuxtLink>
-                <NuxtLink to="#"> GALLERY </NuxtLink>
-                <NuxtLink to="#"> BEASISWA </NuxtLink>
-                <NuxtLink to="#">
-                    <Icon name="uil:shopping-cart" size="20px" />
-                </NuxtLink>
-            </div>
-            <div>
-                <div v-if="!loggedIn" class="flex gap-2 mr-6">
-                    <NuxtLink to="/login">
-                        <UButton color="neutral" class="bg-gray-500"
-                            >Log In</UButton
-                        >
-                    </NuxtLink>
-                    <NuxtLink to="#">
-                        <UButton color="neutral" class="bg-raka-orange"
-                            >Sign Up</UButton
-                        >
-                    </NuxtLink>
-                </div>
-                <div v-else class="flex gap-2 mr-6">
-                    <NuxtLink to="/logout">
-                        <UButton color="neutral" class="bg-raka-red"
-                            >Log Out</UButton
-                        >
-                    </NuxtLink>
-                </div>
-            </div>
-        </div>
+  <!-- Desktop Navigation -->
+  <div v-else class="grid grid-cols-3 w-full h-24 bg-white">
+    <div class="flex items-center">
+      <NuxtLink to="/">
+        <NuxtImg src="/img/logo.png" width="32" class="ml-6" />
+      </NuxtLink>
     </div>
+    <div />
+    <div class="flex justify-end items-center">
+      <div class="flex h-full justify-center items-center gap-3 mx-4">
+        <NuxtLink to="#"> PROGRAM </NuxtLink>
+        <NuxtLink to="#"> GALLERY </NuxtLink>
+        <NuxtLink to="#"> BEASISWA </NuxtLink>
+        <NuxtLink to="#">
+          <Icon name="uil:shopping-cart" size="20px" />
+        </NuxtLink>
+      </div>
+      <div>
+        <div v-if="!loggedIn" class="flex gap-2 mr-6">
+          <NuxtLink to="/login">
+            <UButton color="neutral" class="bg-gray-500">Log In</UButton>
+          </NuxtLink>
+          <NuxtLink to="#">
+            <UButton color="neutral" class="bg-raka-orange">Sign Up</UButton>
+          </NuxtLink>
+        </div>
+        <div v-else class="flex gap-2 mr-6">
+          <NuxtLink to="/dashboard">
+            <UButton color="neutral" class="bg-raka-orange">Dashboard</UButton>
+          </NuxtLink>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
--- app/components/SideBar.vue
+++ app/components/SideBar.vue
@@ -2,90 +2,84 @@
 const { loggedIn } = useUserSession();
 
 const sideItems = ref([
-    {
-        title: "PENGGUNA",
-        child: [
-            {
-                title: loggedIn.value ? "Logout" : "Login",
-                icon: "ic:outline-account-circle",
-                to: loggedIn.value ? "/logout" : "/login",
-            },
-            {
-                title: "Profil",
-                icon: "ic:outline-account-circle",
-                to: "",
-                mustLoggedIn: true,
-            },
-        ],
-    },
-    {
-        title: "MENU UTAMA",
-        child: [
-            {
-                title: "Beranda",
-                icon: "ic:baseline-monitor",
-                to: "/dashboard",
-            },
-            {
-                title: "Katalog Kelas",
-                icon: "ic:outline-auto-awesome-mosaic",
-                to: "",
-            },
-            {
-                title: "Kelas Saya",
-                icon: "ic:outline-school",
-                to: "",
-            },
-            {
-                title: "Pertanyaan",
-                icon: "ic:outline-speaker-notes",
-                to: "",
-            },
-        ],
-    },
+  {
+    title: "PENGGUNA",
+    child: [
+      {
+        title: loggedIn.value ? "Logout" : "Login",
+        icon: "ic:outline-account-circle",
+        to: loggedIn.value ? "/logout" : "/login",
+      },
+      {
+        title: "Profil",
+        icon: "ic:outline-account-circle",
+        to: "",
+        mustLoggedIn: true,
+      },
+    ],
+  },
+  {
+    title: "MENU UTAMA",
+    child: [
+      {
+        title: "Beranda",
+        icon: "ic:baseline-monitor",
+        to: "/dashboard",
+      },
+      {
+        title: "Katalog Kelas",
+        icon: "ic:outline-auto-awesome-mosaic",
+        to: "",
+      },
+      {
+        title: "Kelas Saya",
+        icon: "ic:outline-school",
+        to: "",
+      },
+      {
+        title: "Pertanyaan",
+        icon: "ic:outline-speaker-notes",
+        to: "",
+      },
+    ],
+  },
 ]);
 </script>
 
 <template>
+  <div
+    class="min-h-full min-w-full md:min-w-xs md:max-w-md flex flex-col items-center py-12 bg-transparent bg-white shadow-md"
+  >
+    <NuxtLink to="/">
+      <NuxtImg src="img/logo.png" class="max-w-8" />
+    </NuxtLink>
     <div
-        class="min-h-full min-w-full md:min-w-xs md:max-w-md flex flex-col items-center py-12 bg-transparent md:bg-white md:drop-shadow-gray-900 md:drop-shadow-md"
+      class="flex flex-col min-w-full min-h-full my-10 px-12 space-y-8 overflow-y-scroll"
     >
-        <NuxtLink to="/">
-            <NuxtImg src="img/logo.png" class="max-w-8" />
-        </NuxtLink>
-        <div
-            class="flex flex-col min-w-full min-h-full my-10 px-12 space-y-8 overflow-y-scroll"
-        >
-            <div v-for="menu in sideItems" :key="menu.title" class="w-full">
-                <p class="font-display font-black tracking-wider">
-                    {{ menu.title }}
-                </p>
-                <div class="w-full mt-2">
-                    <div v-for="item in menu.child" :key="item.title">
-                        <NuxtLink
-                            v-if="
-                                item.mustLoggedIn
-                                    ? loggedIn
-                                        ? true
-                                        : false
-                                    : true
-                            "
-                            :to="item.to"
-                        >
-                            <UButton
-                                :icon="item.icon"
-                                size="md"
-                                color="neutral"
-                                variant="solid"
-                                class="bg-transparent text-gray-700 px-4 w-full text-md"
-                                >{{ item.title }}</UButton
-                            >
-                        </NuxtLink>
-                    </div>
-                </div>
-            </div>
+      <div v-for="menu in sideItems" :key="menu.title" class="w-full">
+        <p class="font-display font-black tracking-wider">
+          {{ menu.title }}
+        </p>
+        <div class="w-full mt-2">
+          <div v-for="item in menu.child" :key="item.title">
+            <NuxtLink
+              v-if="item.mustLoggedIn ? (loggedIn ? true : false) : true"
+              :to="item.to"
+            >
+              <UButton
+                :icon="item.icon"
+                size="md"
+                color="neutral"
+                variant="solid"
+                class="bg-transparent text-gray-700 px-4 w-full text-md"
+                >{{ item.title }}</UButton
+              >
+            </NuxtLink>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <style scoped></style>
